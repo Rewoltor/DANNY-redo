1d066decb8be0ce617dd57b8aee30770631e77bc
Marci <baltay.marton@gmail.com>
2025-10-26 10:34:02 +0100

    firebase and .gitigore

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { firebaseSecret } from '../secrets';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyBIKyohpoENAAmXcWwu7Q8pOjonRvy5g_E',
-  authDomain: 'danny-redo.firebaseapp.com',
-  projectId: 'danny-redo',
-  storageBucket: 'danny-redo.firebasestorage.app',
-  messagingSenderId: '861664122007',
-  appId: '1:861664122007:web:75c78636338e2f9b4d05d2',
+  apiKey: firebaseSecret.apiKey,
+  authDomain: firebaseSecret.authDomain,
+  projectId: firebaseSecret.projectId,
+  storageBucket: firebaseSecret.storageBucket,
+  messagingSenderId: firebaseSecret.messagingSenderId,
+  appId: firebaseSecret.appId,
 };
 
 // Initialize Firebase
